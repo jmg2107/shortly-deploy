16be1c9761aa4489f424628a22ebe084e28a86e4
jmg2107 <jenjengoo@gmail.com>
2016-07-08 10:56:38 -0700

    now works w/ MongoDB

--- app/models/link.js
+++ app/models/link.js
@@ -1,19 +1,40 @@
-var db = require('../config');
+// var db = require('../config');
+// var crypto = require('crypto');
+
+// var Link = db.Model.extend({
+//   tableName: 'urls',
+//   hasTimestamps: true,
+//   defaults: {
+//     visits: 0
+//   },
+//   initialize: function(){
+//     this.on('creating', function(model, attrs, options){
+//       var shasum = crypto.createHash('sha1');
+//       shasum.update(model.get('url'));
+//       model.set('code', shasum.digest('hex').slice(0, 5));
+//     });
+//   }
+// });
+
+// module.exports = Link;
+var mongoose = require('mongoose');
+var Link = require('../config').Link;
 var crypto = require('crypto');
 
-var Link = db.Model.extend({
-  tableName: 'urls',
-  hasTimestamps: true,
-  defaults: {
-    visits: 0
-  },
-  initialize: function(){
-    this.on('creating', function(model, attrs, options){
-      var shasum = crypto.createHash('sha1');
-      shasum.update(model.get('url'));
-      model.set('code', shasum.digest('hex').slice(0, 5));
-    });
-  }
-});
 
-module.exports = Link;
+var Sha = function(url){
+  var shasum = crypto.createHash('sha1');
+    shasum.update(url);
+    return shasum.digest('hex').slice(0, 5);
+
+};
+Link.pre('save', function(next){
+  var code = Sha(this.url);
+  this.code = code;
+  next();
+
+})
+
+var Links = mongoose.model('Link', Link);
+
+module.exports = Links;
